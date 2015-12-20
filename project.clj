444288af87ace767560302b177c82421e3de1682
SimonWailand <melancholyrb@gmail.com>
2015-12-19 22:11:31 -0500

    Added EPL info and updated version

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject hexwrench "0.1.1"
+(defproject hexwrench "0.2.0"
   :description "Tools for manipulating hex grids."
   :url "https://github.com/SimonWailand/hexwrench"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                 #_(:cljs [org.clojure/clojurescript "1.7.145"]
-                           [reagent "0.5.1"])]
+                         [reagent "0.5.1"])]
 
   :source-paths ["src/clj"
                  "src/cljc"
