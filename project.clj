901f4be1cf42e4a49ae0811bb410f4ae8a383db0
SimonWailand <melancholyrb@gmail.com>
2016-01-31 23:22:21 -0500

    Updated version, removed unnecessary deps

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject hexwrench "0.2.1"
+(defproject hexwrench "0.2.2"
   :description "Tools for manipulating hex grids."
   :url "https://github.com/SimonWailand/hexwrench"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                #_(:cljs [org.clojure/clojurescript "1.7.145"]
-                         [reagent "0.5.1"])]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
 
   :plugins [[lein-kibit "0.1.2"]]
 
